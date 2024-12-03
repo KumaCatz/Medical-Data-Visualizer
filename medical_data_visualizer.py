3e72a05cd7fdae5e13e0f3fef172b48a38bcc26d
KumaCatz K <tod.kagan@gmail.com>
2024-12-03 23:08:04 +0000

    WIP: debugging df_cat

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -5,8 +5,6 @@ import numpy as np
 
 # 1
 df = pd.read_csv('medical_examination.csv', index_col=0)
-# print(df)
-# ^ delete when finish
 
 # 2
 df['overweight'] = df['weight'] / ((df['height'] / 100) ** 2) > 25
@@ -15,17 +13,16 @@ df['overweight'] = df['overweight'].astype(int)
 # 3
 df['cholesterol'] = np.where(df['cholesterol'] > 1, 1, 0)
 df['gluc'] = np.where(df['gluc'] > 1, 1, 0)
-print(df)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars='cardio', value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], var_name='feature', value_name='value')
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio', 'feature']).size().reset_index(name='counts')
+    print(df_cat)
+    # the values are identical one to the other. theres a bug somewhere in the grouping process
 
     # 7
 
@@ -39,7 +36,7 @@ def draw_cat_plot():
     fig.savefig('catplot.png')
     return fig
 
-
+draw_cat_plot()
 # 10
 def draw_heat_map():
     # 11
